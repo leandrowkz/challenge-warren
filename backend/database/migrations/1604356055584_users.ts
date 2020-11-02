18848f8c84203a9b8dba20b8c7df5d10e9356378
Leandro <leandrowkz@gmail.com>
2020-11-02 19:30:01 -0300

    feat(back): add users table

---
+++ backend/database/migrations/1604356055584_users.ts
@@ -0,0 +1,19 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.timestamps(true)
+      table.string('id', 25).notNullable().index()
+      table.string('name').notNullable()
+      table.string('email').notNullable().unique().index()
+      table.string('password').notNullable()
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
